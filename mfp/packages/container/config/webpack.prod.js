3e9da85e3ad1392e715aeaed8f23da7dd65065a4
Firstname Lastname <you@example.com>
2021-02-26 10:35:09 -0800

    Adding header

--- mfp/packages/container/config/webpack.prod.js
+++ mfp/packages/container/config/webpack.prod.js
@@ -8,13 +8,14 @@ const domain = process.env.PRODUCTION_DOMAIN;
 const prodConfig = {
     mode: 'production',
     output: {
-        filename: '[name].[contenthash].js'
+        filename: '[name].[contenthash].js',
+        publicPath: '/container/latest/'
     },
     plugins: [
         new ModuleFederationPlugin({
             name: 'container',
             remotes: {
-                marketing: `marketing@${domain}/marketing/remoteEntry.js`
+                marketing: `marketing@${domain}/marketing/latest/remoteEntry.js`
             },
             shared: packageJson.dependencies
         })
